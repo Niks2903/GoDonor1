6387d22700cd3e26bfb28d4c5dab0880d12a6d15
Nikhil Patil <niks.patil.2903@gmail.com>
2018-04-14 15:55:38 +0530

    android

---
+++ app/src/main/java/com/example/android/godonor/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.godonor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        ImageView searchView = (ImageView) findViewById(R.id.search_view);
+
+
+
+            // Set a click listener on that View
+
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent searchIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
